b96dc40cd31fd0bd3278f14d7714fddff885927c
Jishnu <jscode99@gmail.com>
2021-04-03 21:45:59 +0530

    Input text state changes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,25 @@
+import React, { useState } from "react";
 import "./App.css";
 
+//Importing Components
+import Form from "./components/Form";
+import Todo from "./components/TodoList";
+
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>My Todo List</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        inputText={inputText}
+      />
+      <Todo />
     </div>
   );
 }
